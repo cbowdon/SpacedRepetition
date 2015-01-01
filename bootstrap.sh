b3faef6b1a8377515b82edb8c2c267c9b434f174
cbowdon <cbowdon@users.noreply.github.com>
2015-01-01 21:22:12 +0000

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Add the Xamarin Mono package repository
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | tee /etc/apt/sources.list.d/mono-xamarin.list
+
+apt-get update
+apt-get install -y vim git mono-complete fsharp
+
+# Vim
+mkdir -p /home/vagrant/.vim/autoload /home/vagrant/.vim/bundle
+cp -u /vagrant/.vimrc /home/vagrant
+
+# Vim: pathogen
+curl -LSso /home/vagrant/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Vim: syntastic
+if [[ ! -d /home/vagrant/.vim/bundle/syntastic ]]; then
+    git clone https://github.com/scrooloose/syntastic.git /home/vagrant/.vim/bundle/syntastic
+fi
+
+# Vim: F# binding
+if [[ ! -d /home/vagrant/fsharpbinding ]]; then
+    git clone https://github.com/fsharp/fsharpbinding.git /home/vagrant
+fi
+cd /home/vagrant/fsharpbinding/vim
+make install
+
+git config --global user.name "cbowdon"
+git config --global user.email "cbowdon@users.noreply.github.com"
